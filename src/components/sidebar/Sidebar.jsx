bd0c15cd91e2b3e8b8f30cab41953e0db61847c1
Sarah Hepworth <sarah.hepworth@bunches.co.uk>
2024-01-12 15:06:58 +0000

    Nav bar and some editor config

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,16 +1,38 @@
 import "./sidebar.scss";
 import ToggleButton from "./toggleButton/ToggleButton.jsx";
 import Links from "./links/Links.jsx";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  const variants = {
+    open: {
+      clipPath: "circle(1200px at 50px 50px)",
+      transition: {
+        type: "spring",
+        stiffness: 20
+      }
+    },
+    closed: {
+      clipPath: "circle(20px at 50px 50px)",
+      transition: {
+        delay: 0.5,
+        type: "spring",
+        stiffness: 400,
+        damping: 40
+      }
+    }
+  };
   return (
-    <div className="sidebar">
-      <div className="background">
+    <motion.div className="sidebar" animate={open ? "open" : "closed"}>
+      <motion.div className="background" variants={variants}>
         <Links />
-      </div>
-      <ToggleButton />
-    </div>
-  )
-}
+      </motion.div>
+      <ToggleButton setOpen={setOpen} open={open} />
+    </motion.div>
+  );
+};
 
 export default Sidebar;
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,5 +1,15 @@
 const Links = () => {
-  return <div>Links</div>
+  const items = [
+    "Homepage",
+    "Services",
+    "Portfolio",
+    "About",
+    "Contact"
+  ];
+
+  return <div className="links">{items.map((item => (
+    <a key={item} href={`#${item}`}>{item}</a>
+  )))}</div>
 }
 
 export default Links
