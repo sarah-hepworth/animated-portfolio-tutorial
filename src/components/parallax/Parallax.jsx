f81cf1326ed1b5afd21b578b2c0f267d663b8ab4
Sarah Hepworth <sarah.hepworth@bunches.co.uk>
2024-03-15 14:48:08 +0000

    add paralax css|

---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,33 @@
+import "./parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+
+const Parallax = ({ type }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref
+  });
+
+  const yBg = useTransform(scrollYProgress);
+
+  return (
+    <div
+      className="parallax"
+      ref={ref}
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)"
+      }}
+    >
+      <motion.h1>{type === "services" ? "What We Do?" : "What We Did?"}</motion.h1>
+      <motion.div className="mountains"></motion.div>
+      <motion.div className="planets"></motion.div>
+      <motion.div className="stars"></motion.div>
+    </div>
+  );
+};
+
+export default Parallax;
