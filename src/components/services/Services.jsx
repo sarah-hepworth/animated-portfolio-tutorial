e27f34bdd1ed2f6c5432fd8160011efff887c787
Sarah Hepworth <sarah.hepworth@bunches.co.uk>
2024-03-22 13:32:58 +0000

    add scroll animation

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,32 +1,52 @@
 import "./services.scss";
-import { motion } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
-const variants = {};
+const variants = {
+  initial: {
+    x: -500,
+    y: 100,
+    opacity: 0
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1
+    }
+  }
+};
 
 const Services = () => {
+  const ref = useRef();
+
+  const isInView = useInView(ref, { margin: "-100px" });
+
   return (
-    <motion.div className="services">
-      <motion.div className="textContainer">
+    <motion.div className="services" ref={ref} variants={variants} initial="initial" animate={isInView && "animate"}>
+      <motion.div className="textContainer" variants={variants}>
         <p>
           I focus on helping your brand grow <br /> and move forward
         </p>
         <hr></hr>
       </motion.div>
-      <motion.div className="titleContainer">
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
           <img src="/people.webp" alt="" />
           <h1>
-            <b>Unique</b> Ideas
+            <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <b>For Your</b> Business
+            <motion.b whileHover={{ color: "orange" }}>For Your</motion.b> Business
           </h1>
           <button>WHAT WE DO</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer">
+      <motion.div className="listContainer" variants={variants}>
         <motion.div className="box" whileHover={{ background: "lightgray", color: "black" }}>
           <h2>Branding</h2>
           <p>
